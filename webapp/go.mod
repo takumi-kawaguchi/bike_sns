441c86b6aad8dae51c42e8ecea7b5af0a2c91252
Takumi Kawaguchi <tcgds4@gmail.com>
2021-12-02 23:16:42 +0900

    [update]ルーティング設定追加

--- webapp/go.mod
+++ webapp/go.mod
@@ -5,4 +5,11 @@ go 1.13
 require (
 	github.com/gin-contrib/multitemplate v0.0.0-20210428235909-8a2f6dd269a0
 	github.com/gin-gonic/gin v1.7.4
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- webapp/main.go
+++ webapp/main.go
@@ -10,14 +10,30 @@ import (
 func main() {
 	router := gin.Default()
 	router.HTMLRender = loadTemplates("./templates")
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(200, "home.html", gin.H{
-			"title": "タイトル：ホーム",
+	router.GET("/timeline", func(c *gin.Context) {
+		c.HTML(200, "timeline.html", gin.H{
+			"title": "タイムライン",
 		})
 	})
-	router.GET("/postlist", func(c *gin.Context) {
-		c.HTML(200, "postlist.html", gin.H{
-			"title": "タイトル：投稿リスト",
+	router.GET("/post", func(c *gin.Context) {
+		c.HTML(200, "post.html", gin.H{
+			"title": "投稿画面",
+		})
+	})
+	router.GET("/post/detail/:id", func(c *gin.Context) {
+		c.HTML(200, "post_detail.html", gin.H{
+			"title": "投稿詳細",
+			"id":    c.Param("id"),
+		})
+	})
+	router.GET("/search_bike", func(c *gin.Context) {
+		c.HTML(200, "search_bike.html", gin.H{
+			"title": "バイク検索",
+		})
+	})
+	router.GET("/search_result", func(c *gin.Context) {
+		c.HTML(200, "search_result.html", gin.H{
+			"title": "検索結果",
 		})
 	})
 
@@ -42,7 +58,8 @@ func loadTemplates(templatesDir string) multitemplate.Renderer {
 		layoutCopy := make([]string, len(layouts))
 		copy(layoutCopy, layouts)
 		files := append(layoutCopy, include)
-		r.AddFromFiles(filepath.Base(include), files...)
+		test := filepath.Base(include)
+		r.AddFromFiles(test, files...)
 	}
 	return r
 }
