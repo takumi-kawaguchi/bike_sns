ae6a4a8dc021086071cbd28dda83ed56cad56127
Takumi Kawaguchi <tcgds4@gmail.com>
2022-02-27 14:42:48 +0900

    [update]docker-composeファイル修正

--- scraping/databasetest.py
+++ scraping/databasetest.py
@@ -1,1 +1,14 @@
-import mysql.connector
+import mysql.connector as mydb
+
+conn = mydb.connect(
+    host='bike-sns-db-container',
+    port=3307,
+    user='test',
+    password='bike_sns',
+    database='bike_sns'
+)
+
+print(conn.is_connected())
+
+
+# bike_makers
