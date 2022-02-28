411b1ab12f677fb5ae4b01d440ff3cdba359e0fc
Takumi Kawaguchi <tcgds4@gmail.com>
2022-02-28 23:58:09 +0900

    [update]バッチコンテナからDBコンテナにSQL叩いて帰ってくるところまで作成

--- scraping/databasetest.py
+++ scraping/databasetest.py
@@ -1,14 +1,30 @@
-import mysql.connector as mydb
+import mysql.connector as mysql
 
-conn = mydb.connect(
-    host='bike-sns-db-container',
-    port=3307,
-    user='test',
-    password='bike_sns',
-    database='bike_sns'
-)
 
-print(conn.is_connected())
+# conn = mydb.connect(
+#     host='bike-sns-db-container',
+#     port=3307,
+#     user='test',
+#     password='bike_sns',
+#     database='bike_sns'
+# )
+
+# print(conn.is_connected())
 
 
 # bike_makers
+
+conn = mysql.connect(
+    host="bike-sns-db-container",
+    user="root",
+    port=3306,
+    database="bike_sns"
+)
+
+cur = conn.cursor()
+
+table = 'users'
+cur.execute('SELECT * FROM users')
+rows = cur.fetchall()
+for row in rows:
+    print(row)
